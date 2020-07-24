c8ae06b5b91c483e32508f6ec69ee3bcdc87f44f
Adedipupo <bayonleolaluwa@gmail.com>
2020-07-24 02:47:34 +0100

    log

--- script.js
+++ script.js
@@ -17,15 +17,16 @@ class Record {
 
   const record1 = new Record();
 
-record1.addEvent  
+record1.addEvent('Running', 1);
+record1.addEvent('Drinking', 1);
+record1.addEvent('Work', 1);
+record1.addEvent('Touched Oak tree', 1);  
+record1.addEvent('Peanuts', 2);
+record1.addEvent('Brushed teeth', 2);
 addEntry(["work", "touched tree", "pizza", "running",
     "television"], false);
 addEntry(["work", "ice cream", "cauliflower", "lasagna",
     "touched tree", "brushed teeth"], false);
 addEntry(["weekend", "cycling", "break", "peanuts",
     "beer"], true);
-record1.addEvent('Drinking', 1);
-record1.addEvent('Work', 1);
-record1.addEvent('Touched Oak tree', 1);
-record1.addEvent('Peanuts', 2);
-record1.addEvent('Brushed teeth', 2);
+
