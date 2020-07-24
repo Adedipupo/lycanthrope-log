9656e477631930710187d36dc315230724107925
Adedipupo <bayonleolaluwa@gmail.com>
2020-07-24 02:44:14 +0100

    log

--- script.js
+++ script.js
@@ -16,7 +16,12 @@ class Record {
 }
 
   const record1 = new Record();
-
+addEntry(["work", "touched tree", "pizza", "running",
+    "television"], false);
+addEntry(["work", "ice cream", "cauliflower", "lasagna",
+    "touched tree", "brushed teeth"], false);
+addEntry(["weekend", "cycling", "break", "peanuts",
+    "beer"], true);
 record1.addEvent('Drinking', 1);
 record1.addEvent('Work', 1);
 record1.addEvent('Touched Oak tree', 1);
