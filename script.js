0d72a72f6b87f676f058a1be3ae2ec19eab43c16
Adedipupo <bayonleolaluwa@gmail.com>
2020-07-24 02:54:58 +0100

    log

--- script.js
+++ script.js
@@ -18,7 +18,7 @@ class Record {
   const record1 = new Record();
 
 record1.addEvent('Running', 1);
-record1.addEvent('Drinking', 1);
+record1.addEvent('Pizza', 1);
 record1.addEvent('Work', 1);
 record1.addEvent('Touched Oak tree', 2);  
 record1.addEvent('Peanuts', 2);
@@ -41,7 +41,7 @@ record1.addEvent('Running', 7);
 record1.addEvent('Drinking', 8);
 record1.addEvent('Work', 8);
 record1.addEvent('Touched Oak tree', 8);
-record1.addEvent('Peanuts', 9);
+record1.addEvent('Pizza', 9);
 record1.addEvent('Brushed teeth', 9);
 record1.addEvent('Peanuts', 9);
 record1.addEvent('Brushed teeth', 10);
